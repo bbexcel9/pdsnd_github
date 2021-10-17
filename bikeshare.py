ff6a73dae8a9f42e8a6842912c4bd6bb7691d807
Omobolaji Solomon <omobolaji.solo@gmail.com>
2021-10-17 03:43:18 +0100

    Clue for city user input

--- bikeshare.py
+++ bikeshare.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Howdy! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # Fetch user input for city (chicago, new york city, washington). Clue: Use a while loop to handle invalid inputs
     while True:
         city = input("Which city would you like to explore ?")
         city = city.lower()
@@ -25,7 +25,7 @@ def get_filters():
         else:
             print("invalid input. Please enter a valid input")
     # get user input for month (all, january, february, ... , june)
-    while True:    
+    while True:
         month = input("Do you want details specific to a particular month? If yes, type month name from within first six months otherwise type 'all'")
         month = month.lower()
         if month in ['january', 'february', 'march', 'april', 'may', 'june', 'all']:
@@ -57,7 +57,7 @@ def load_data(city, month, day):
     """
      # load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
-    
+
     # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
@@ -71,14 +71,14 @@ def load_data(city, month, day):
         # use the index of the months list to get the corresponding int
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
-    
+
         # filter by month to create the new dataframe
         df = df[df['month'] == month]
 
     # filter by day of week if applicable
     if day != 'all':
         # filter by day of week to create the new dataframe
-        
+
         df = df[df['day_of_week'] == day.title()]
 
     return df
@@ -190,4 +190,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-    
